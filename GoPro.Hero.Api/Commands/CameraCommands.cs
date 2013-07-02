0483659107ad5d101cdbf0158cf38b6812939601
r1pper <the.r1pper@hotmail.com>
2013-07-02 23:05:46 +0430

    Commands Refactored

--- GoPro.Hero.Api/Commands/CameraCommands.cs
+++ GoPro.Hero.Api/Commands/CameraCommands.cs
@@ -8,54 +8,30 @@ namespace GoPro.Hero.Api.Commands.CameraCommands
     [Command(HeroCommands.CAMERA_EXTENDED_SETTINGS, Parameterless = true)]
     class CommandCameraExtendedSettings : CommandRequest
     {
-        public static CommandCameraExtendedSettings Create(string address, string passPhrase)
-        {
-            return CommandRequest.Create<CommandCameraExtendedSettings>(address, HeroCommands.CAMERA_EXTENDED_SETTINGS, passPhrase);
-        }
     }
 
     [Command(HeroCommands.CAMREA_INFORMATION, Parameterless = true)]
     class CommandCameraInformation : CommandRequest
     {
-        public static CommandCameraInformation Create(string address, string passPhrase)
-        {
-            return CommandRequest.Create<CommandCameraInformation>(address, HeroCommands.CAMREA_INFORMATION, passPhrase);
-        }
     }
 
     [Command(HeroCommands.CAMREA_LOCATE)]
     class CommandCameraLocate : CommandBoolean
     {
-        public static CommandCameraLocate Create(string address, string passPhrase)
-        {
-            return CommandRequest.Create<CommandCameraLocate>(address, HeroCommands.CAMREA_LOCATE, passPhrase);
-        }
     }
 
     [Command(HeroCommands.CAMREA_EXPOSURE)]
     class CommandCameraProtune : CommandBoolean
     {
-        public static CommandCameraSpotMeter Create(string address, string passPhrase)
-        {
-            return CommandRequest.Create<CommandCameraSpotMeter>(address, HeroCommands.CAMREA_EXPOSURE, passPhrase);
-        }
     }
 
     [Command(HeroCommands.CAMERA_SETTINGS, Parameterless = true)]
     class CommandCameraSettings : CommandRequest
     {
-        public static CommandCameraExtendedSettings Create(string address, string passPhrase)
-        {
-            return CommandRequest.Create<CommandCameraExtendedSettings>(address, HeroCommands.CAMERA_SETTINGS, passPhrase);
-        }
     }
 
     [Command(HeroCommands.CAMREA_EXPOSURE)]
     class CommandCameraSpotMeter : CommandBoolean
     {
-        public static CommandCameraSpotMeter Create(string address, string passPhrase)
-        {
-            return CommandRequest.Create<CommandCameraSpotMeter>(address, HeroCommands.CAMREA_EXPOSURE, passPhrase);
-        }
     }
 }
