c6eb0264d83b39df31d282543593407b8628e8e2
r1pper <the.r1pper@hotmail.com>
2013-07-06 10:14:18 +0430

    Filtering interface defined

---
+++ GoPro.Hero.Api/Filtering/IFiltering.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GoPro.Hero.Api.Commands;
+
+namespace GoPro.Hero.Api.Filtering
+{
+    interface IFiltering<O>
+    {
+        bool IsAvailable(CommandRequest<O> command);
+        bool IsAvailable<C>() where C : CommandRequest<O>;
+        O Available<C>(out bool state) where C : CommandRequest<O>;
+
+        IEnumerable<T> GetValidStates<T>(CommandMultiChoice<T, O> command);
+        IEnumerable<T> GetValidStates<T, C>() where C : CommandMultiChoice<T, O>;
+        O ValidStates<T, C>(out IEnumerable<T> validStates) where C : CommandMultiChoice<T, O>;
+
+        IEnumerable<bool> GetValidStates(CommandBoolean<O> command);
+        IEnumerable<bool> GetValidStates<C>() where C : CommandBoolean<O>;
+        O ValidStates<C>(out IEnumerable<bool> validStates) where C : CommandBoolean<O>;
+
+        bool IsValid(CommandRequest<O> command);
+        O Valid(CommandRequest<O> command, out bool state);
+    }
+}
