6b42345bdf1158d5b342b7d3ec309d1e3158e3ea
r1pper <hessam.jalali@outlook.com>
2016-05-05 11:25:05 +0430

    start working on hero4

---
+++ src/GoPro.Hero/Hero4/Hero4Commands.cs
@@ -0,0 +1,64 @@
+﻿namespace GoPro.Hero.Hero4
+{
+    class Hero4Commands
+    {
+        public const string CAMERA_PROTUNE_VIDEO = "/gp/gpControl/setting/10/";
+        public const string CAMERA_PROTUNE_PHOTO= "/gp/gpControl/setting/21/";
+        public const string CAMERA_PROTUNE_MULTISHOT = "/gp/gpControl/setting/34/";
+
+        public const string CAMERA_WHITEBALANCE_VIDEO = "/gp/gpControl/setting/11/";
+        public const string CAMERA_WHITEBALANCE_PHOTO = "/gp/gpControl/setting/22/";
+        public const string CAMERA_WHITEBALANCE_MULTISHOT = "/gp/gpControl/setting/35/";
+
+        public const string CAMERA_COLOR_VIDEO = "/gp/gpControl/setting/12/";
+        public const string CAMERA_COLOR_PHOTO = "/gp/gpControl/setting/23/";
+        public const string CAMERA_COLOR_MULTISHOT = "/gp/gpControl/setting/36/";
+
+        public const string CAMERA_ISO_VIDEO = "/gp/gpControl/setting/13/";
+        public const string CAMERA_ISO_PHOTO = "/gp/gpControl/setting/24/";
+        public const string CAMERA_ISO_MULTISHOT = "/gp/gpControl/setting/37/";
+
+        public const string CAMERA_SHARPNESS_VIDEO = "/gp/gpControl/setting/14/";
+        public const string CAMERA_SHARPNESS_PHOTO = "/gp/gpControl/setting/25/";
+        public const string CAMERA_SHARPNESS_MULTISHOT = "/gp/gpControl/setting/38/";
+
+        public const string CAMERA_EV_VIDEO = "/gp/gpControl/setting/15/";
+        public const string CAMERA_EV_PHOTO = "/gp/gpControl/setting/26/";
+        public const string CAMERA_EV_MULTISHOT = "/gp/gpControl/setting/39/";
+
+        public const string CAMERA_DEFAULT_BOOTMODE = "/gp/gpControl/setting/53/";
+        public const string CAMERA_PRIMARY_MODE = "/gp/gpControl/command/mode";
+
+        public const string CAMERA_POWER_OFF = "/gp/gpControl/command/system/sleep";
+        public const string CAMERA_SHUTTER = "/gp/gpControl/command/shutter";
+
+        public const string CAMERA_ORIENTATION = "/gp/gpControl/setting/52/";
+
+        public const string CAMERA_QUICK_CAPTURE = "/gp/gpControl/setting/54/";
+
+        public const string CAMERA_LED_BLINK = "/gp/gpControl/setting/55/";
+
+        public const string CAMERA_BEEPS = "/gp/gpControl/setting/56/";
+
+        public const string CAMERA_VIDEO_FORMAT = "/gp/gpControl/setting/57/";
+
+        public const string CAMERA_LCD_DISPLAY = "/gp/gpControl/setting/72/";
+
+        public const string CAMERA_ONSCREEN_DISPLAY = "/gp/gpControl/setting/58/";
+
+        public const string CAMERA_AUTO_OFF = "/gp/gpControl/setting/59/";
+
+        public const string CAMERA_BITSTREAMING = "/gp/gpControl/setting/62/";
+
+        public const string CAMRERA_TAG = "/gp/gpControl/command/storage/tag_moment";
+
+        public const string CAMRERA_PAIR_RC = "/setting/63/2";
+
+        public const string CAMERA_LOCATE= "/gp/gpControl/command/system/locate";
+
+        public const string CAMERA_DELETE_LAST = "/gp/gpControl/command/storage/delete/last";
+
+        public const string CAMERA_DELETE_ALL = "/gp/gpControl/command/storage/delete/all";
+
+    }
+}
---
+++ src/GoPro.Hero/Hero4/Structures.cs
@@ -0,0 +1,17 @@
+﻿namespace GoPro.Hero.Hero4
+{
+    public enum StreamBitRate
+    {
+
+        R250Kbps = 250000,
+        R400Kbps400000,
+        R600Kbps = 600000,
+        R700Kbps = 700000,
+        R800Kbps = 800000,
+        R1Mbps = 1000000,
+        R1p2Mbps = 1200000,
+        R1p6Mbps = 1600000,
+        R2Mbps = 2000000,
+        R2p4Mbps = 2400000
+    }
+}
